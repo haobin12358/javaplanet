4c0c4c488967ab0202c1263bda8dd6aedf77ebe8
haobin12358 <1276121237@qq.com>
2020-09-01 16:48:31 +0800

    insert company

--- src/main/java/com/sanbinit/planet/control/CompanyMessageController.java
+++ src/main/java/com/sanbinit/planet/control/CompanyMessageController.java
@@ -2,13 +2,16 @@ package com.sanbinit.planet.control;
 
 import com.github.pagehelper.PageInfo;
 import com.sanbinit.planet.dao.model.CompanyMessage;
+import com.sanbinit.planet.dao.response.Success;
 import com.sanbinit.planet.service.CompanyMessageService;
+import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 //TODO 待封装
 @RestController
@@ -20,15 +23,39 @@ public class CompanyMessageController {
     @Autowired
     private CompanyMessageService companyMessageService;
 
+    Success success = new Success();
+
     //打印用
     protected static final Logger logger = LoggerFactory.getLogger(CompanyMessageController.class);
 
     @GetMapping("/list")
-    public PageInfo<CompanyMessage> getAll(@RequestParam int cmindex, CompanyMessage companyMessage){
+    public PageInfo<CompanyMessage> getAll(@RequestParam int cmindex,
+                                           CompanyMessage companyMessage){
         List<CompanyMessage> companyMessageList = companyMessageService.getAllCompanyMessage(companyMessage, cmindex);
         //logger.error(Integer.toString(cmindex));
         return new PageInfo<CompanyMessage>(companyMessageList);
     }
 
-
+    @PostMapping("/create_message")
+    public Success createOne(@RequestBody CompanyMessage companyMessageResponse,
+                             CompanyMessage companyMessage){
+        companyMessage.setAll(0, DateTime.now(), DateTime.now());
+        companyMessage.setCMid(UUID.randomUUID().toString());
+        companyMessage.setCMtitle(companyMessageResponse.getCMtitle());
+        companyMessage.setCMindex(companyMessageResponse.getCMindex());
+        companyMessage.setCMmessage(companyMessageResponse.getCMmessage());
+        companyMessage.setCMreadnum(0);
+        int code = companyMessageService.createOneCompanyMessage(companyMessage);
+        if(code == 1){
+            success.setStatus(200);
+            success.setMessage("创建成功");
+            success.setStatus_code(200);
+            return success;
+        }else{
+            success.setStatus(405);
+            success.setStatus_code(405001);
+            success.setMessage("创建失败");
+            return success;
+        }
+    }
 }
